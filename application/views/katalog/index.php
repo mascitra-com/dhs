808a583c2b14090c41a6c330ee42869f56d1e957
Rizki Herdatullah <rizkiherda@gmail.com>
2016-10-14 21:58:02 +0700

    Export & Import fix and Default Image included

--- application/views/katalog/index.php
+++ application/views/katalog/index.php
@@ -1,5 +1,5 @@
 <div class="row">
-<?php $this->load->view('katalog/filter'); ?>
+<?php $this->load->view('katalog/filter');?>
 	<div class="col-md-10">
 		<div class="row">
 			<div class="col-md-8">
@@ -7,24 +7,24 @@
 				<button class="btn btn-success btn-md btn-fill" onclick="location.reload()"><i class="fa fa-refresh"></i>Segarkan Tabel <span class="badge"></span></button>
 			</div>
 			<div class="col-md-4 text-right">
-				<button class="btn btn-success btn-fill"><i class="fa fa-file-excel-o"></i></button>
-				<button class="btn btn-danger btn-fill"><i class="fa fa-file-pdf-o"></i></button>
+				<a class="btn btn-success btn-fill" href="<?=site_url("export/katalog/excel5")?>"><i class="fa fa-file-excel-o"></i> Export to Excel</a>
+				<a class="btn btn-danger btn-fill"  href="<?=site_url("export/katalog/pdf")?>"><i class="fa fa-file-pdf-o"></i> Export to PDF</a>
 			</div>
 		</div>
 		<br><br>
 		<div class="row">
-			<?php foreach($data as $key): ?>
+			<?php foreach ($data as $key): ?>
 			<div class="col-sm-6 col-md-4 col-lg-3">
 				<div class="thumbnail" id="barang<?=$key->id?>" style="background-color: #fafafa">
 					<div class="thumbnail-action" style="position:absolute; top:10px; right:25px">
 						<a href="#" class="btn btn-primary btn-fill btn-xs" role="button"><i class="fa fa-pencil"></i></a>
-						<button type="button" class="btn btn-danger btn-fill btn-xs btn-round" role="button" onclick="hapus(<?=$key->id?>, '<?=$key->gambar?>')"><i class="fa fa-trash"></i></button>
+						<button type="button" class="btn btn-danger btn-fill btn-xs btn-round" role="button" onclick="hapus(<?=$key->id?>, '<?=$key->gambar?>')"><i class="fa fa-close"></i></button>
 					</div>
 					<img src="<?=base_url()?>assets/img-user/<?=cek_file($key->gambar)?>" alt="<?=$key->gambar?>">
 					<div class="caption">
 						<div class="row">
 							<div class="col-md-8">
-								<h4 style="margin-top:20px; margin-bottom: 3px"><a href="<?=site_url('katalog/detail/'.$key->id)?>" target='_blank'><?=$key->nama?></a></h4>
+								<h4 style="margin-top:20px; margin-bottom: 3px"><a href="<?=site_url('katalog/detail/' . $key->id)?>" target='_blank'><?=$key->nama?></a></h4>
 							</div>
 							<div class="col-md-4">
 								<div style="margin-top: 10px"><i class="label label-default"><?=$key->kategori?></i></div>
@@ -39,7 +39,7 @@
 								</tr>
 								<tr>
 									<td>Merk</td>
-									<td>&nbsp: <a href="<?=site_url('katalog?merk='.$key->merk)?>"><?=$key->merk?></a></td>
+									<td>&nbsp: <a href="<?=site_url('katalog?merk=' . $key->merk)?>"><?=$key->merk?></a></td>
 								</tr>
 								<tr>
 									<td>Tipe</td>
@@ -50,24 +50,24 @@
 						<p style="margin-top: 20px;">
 						<div class="row">
 							<div class="col-md-3">
-								<a href="<?=site_url('katalog/detail/'.$key->id)?>" target='_blank' class="btn btn-success btn-fill btn-xs" role="button">detail</a>
+								<a href="<?=site_url('katalog/detail/' . $key->id)?>" target='_blank' class="btn btn-success btn-fill btn-xs" role="button">detail</a>
 							</div>
 							<div class="col-md-9 text-right">
 								<h1 style="font-size:19pt; line-height:0; margin: 0; padding-top: 10px;">Rp.<?=$key->hargaSatuan?></h1>
 							</div>
-						</div>	
+						</div>
 						</p>
 					</div>
 				</div>
 			</div>
-			<?php endforeach; ?>
+			<?php endforeach;?>
 		</div>
 	</div>
 </div>
 
 <?php
-function cek_file($filename){
-	if (! file_exists('./assets/img-user/'.$filename) || $filename == '') {
+function cek_file($filename) {
+	if (!file_exists('./assets/img-user/' . $filename) || $filename == '') {
 		$filename = 'default.png';
 	}
 	return $filename;
