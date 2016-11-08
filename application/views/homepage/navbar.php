461dcab368b97386f5ba9b89f0610f0978d071e9
Ainul Yakin <ainul086@gmail.com>
2016-11-08 11:51:26 +0700

    update

--- application/views/homepage/navbar.php
+++ application/views/homepage/navbar.php
@@ -14,12 +14,25 @@
         <div class="navbar-collapse collapse navbar-right" id="navbar-collapsible">
             <ul class="nav navbar-nav navbar-left">
                 <li><a href="<?=site_url()?>">Home</a></li>
-                <li><a href="<?=site_url('homepage/daftar')?>">Katalog</a></li>
+
+                <?php if($this->ion_auth->logged_in()){ ?>
+                     <li><a href="<?=site_url('katalog')?>">Katalog</a></li>
+                <?php }else{ ?>
+                     <li><a href="<?=site_url('homepage/daftar')?>">Katalog</a></li>
+                <?php } ?>
+
+               
                 <li><a href="<?=site_url('homepage/regulasi')?>">Regulasi</a></li>
                 <li><a href="#">Download</a></li>
                 <li><a href="#">Petunjuk</a></li>
                 <li><a href="<?=site_url('homepage/kontak')?>">Hubungi Kami</a></li>
-                <li><a href="<?=site_url('users/login')?>">Login</a></li>
+                
+                <?php if($this->ion_auth->logged_in()){ ?>
+                    <li><a href="<?=site_url('users/logout')?>">Logout</a></li>
+                <?php }else{ ?>
+                    <li><a href="<?=site_url('users/login')?>">Login</a></li>
+                <?php } ?>
+
                 <li>&nbsp;</li>
             </ul>
         </div>
