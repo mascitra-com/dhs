8466a7919d96ba58246d0134749d05c0941a54de
Rizki Herdatullah <rizkiherda@gmail.com>
2016-11-19 15:01:30 +0700

    Fix Mobile Error

--- application/views/homepage/index.php
+++ application/views/homepage/index.php
@@ -5,6 +5,7 @@
 	<title>SISAGA | Sistem Informasi Standar Harga Barang Pemerintah Kab. Lumajang</title>
 	<link rel="icon" type="image/png" href="<?=base_url('assets/img/favicon.png')?>">
 	<?php $this->load->view('homepage/css');?>
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
 </head>
 <body>
 	<?php $this->load->view('homepage/navbar');?>
@@ -14,12 +15,12 @@
 
 	<?php $this->load->view('homepage/footer');?>
 
-	<?php $datestring = '%Y %F %d'; ?>
+	<?php $datestring = '%Y %F %d';?>
 	<div class="pengumuman">
 		<?php foreach ($info as $data): ?>
 			<span>[ <?=mdate($datestring, strtotime($data->createdAt))?> ]</span>
-			<span <?= ($data->penting != 0 || $data->penting != null) ? "class='penting'" : "" ?>><?= $data->isi ?></span>
-		<?php endforeach; ?>
+			<span <?=($data->penting != 0 || $data->penting != null) ? "class='penting'" : ""?>><?=$data->isi?></span>
+		<?php endforeach;?>
 	</div>
 
 	<?php $this->load->view('homepage/javascript');?>
